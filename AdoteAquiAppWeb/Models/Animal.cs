d5262af09e15792cf85035a39443d7f6b85418a9
Daniel-Roch <daniel_roch4@hotmail.com>
2023-11-05 22:57:07 -0300

    chore: implement slug and other modific

--- AdoteAquiAppWeb/Models/Animal.cs
+++ AdoteAquiAppWeb/Models/Animal.cs
@@ -1,17 +1,23 @@
 ﻿namespace AdoteAquiAppWeb.Models;
+using System.ComponentModel.DataAnnotations;
 public class Animal {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string NameSlug => Name.ToLower().Replace(" ","-");
     public string Img { get; set; }
     public string Url { get; set; }
+    public bool Adoption { get; set; }
+    public string AdoptionShadow => Adoption ? "Sim" : "Não";
     public string Description { get; set; }
+    [Display(Name = "Local")]
     public string Andress { get; set; }
 
-    public Animal(int id, string name, string img, string url, string description, string andress) {
+    public Animal(int id, string name, string img, string url, bool adoption, string description, string andress) {
         Id = id;
         Name = name; 
         Img = img; 
-        Url = url; 
+        Url = url;
+        Adoption = adoption;
         Description = description;
         Andress = andress;
     }
